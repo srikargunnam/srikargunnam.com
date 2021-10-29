03e50b124c41e17973e46ee6474a3992693e8284
Srikar-Gunnam <gunnamsri96@gmail.com>
2021-10-29 22:10:56 +0530

    update mod.ts

--- mod.ts
+++ mod.ts
@@ -2,5 +2,5 @@ import { serve, serveStatic } from "https://deno.land/x/sift@0.4.2/mod.ts";
 
 serve({
   "/": serveStatic("index.html", { baseUrl: import.meta.url }),
-  "/style.css": serveStatic("style.css", { baseUrl: import.meta.url }),
+  "/:filename+": serveStatic(".", { baseUrl: import.meta.url }),
 });
