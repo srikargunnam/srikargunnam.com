301067147792c5070d01c851376f199938a689e0
srikargunnam <i@srikargunnam.com>
2025-02-23 09:09:27 +0530

    delete mod.ts used to serve static by deno

--- mod.ts
+++
@@ -1,6 +0,0 @@
-import { serve, serveStatic } from "https://deno.land/x/sift@0.4.2/mod.ts";
-
-serve({
-  "/": serveStatic("index.html", { baseUrl: import.meta.url }),
-  "/:filename+": serveStatic(".", { baseUrl: import.meta.url }),
-});
